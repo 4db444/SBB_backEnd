37e8a5a2f2fd43c97e0df09c16b55d56a4e95592
brahim444 <brahim77990alhyan@gmail.com>
2025-05-22 23:06:57 +0100

    create the groupcontroller and add its routes

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IncomeController;
 use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\GroupController;
 
 
 Route::middleware("auth:sanctum")->group(function(){
+    Route::prefix("/auth")->group(function (){
+        Route::get("/user", [AuthController::class, "show"])->middleware("auth:sanctum");
+        Route::post("/logout", [AuthController::class, "destroy"])->middleware("auth:sanctum"); 
+    });
+
     Route::prefix("/income")->group(function(){
         Route::get("/", [IncomeController::class, "index"]);
         Route::post("/", [IncomeController::class, "store"]);
@@ -21,16 +27,20 @@ Route::middleware("auth:sanctum")->group(function(){
         Route::put("/{id}", [ExpenseController::class, "update"]);
         Route::delete("/{id}", [ExpenseController::class, "destroy"]);
     });
-    
-    Route::prefix("/auth")->group(function (){
-        Route::get("/user", [AuthController::class, "show"])->middleware("auth:sanctum");
-        Route::post("/logout", [AuthController::class, "destroy"])->middleware("auth:sanctum"); 
+
+    Route::prefix("/group")->group(function(){
+        Route::get("/", [GroupController::class, "index"]);
+        Route::post("/", [GroupController::class, "store"]);
+        Route::get("/{id}/members", [GroupController::class, "members"]);
+        Route::post("/{id}/generate", [GroupController::class, "generate_token"]);
+        Route::get("/join/{join_token}", [GroupController::class, "join"]);
     });
+    
 });
 
 Route::middleware("guest")->group(function (){
     Route::prefix("/auth")->group(function (){
-        Route::post("/register", [AuthController::class, "store"])->middleware("guest");
-        Route::post("/login", [AuthController::class, "login"])->middleware("guest");
+        Route::post("/register", [AuthController::class, "store"]);
+        Route::post("/login", [AuthController::class, "login"]);
     });
 });
